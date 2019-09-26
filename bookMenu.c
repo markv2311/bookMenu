1a9b6bdc63a1036f4a2ef9385784f856fdf93fc2
Mark Valeriani <markv2311@gmail.com>
2019-09-26 17:06:28 -0400

    Intial commit of notes

---
+++ bookMenu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+
+int main (int argv, char *args){
+    //use fgets to get the books 
+    //set up two parallel arrarys 
+    //one to hold books 
+    //one to hold prices 
+    //no more then ten books and 
+    //no more than 100 characters
+      
+
+}
